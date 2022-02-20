1659c8eef35836864dd394994999b374c3613e69
Ayush Sharma <ayushsharmausa@gmail.com>
2022-02-20 08:52:52 -0600

    refactor

--- driver_evolution/math/collision.py
+++ driver_evolution/math/collision.py
@@ -1,3 +1,11 @@
+def limit(lower, upper, num):
+    if num < lower:
+        return lower
+    elif num > upper:
+        return upper
+    return num
+
+
 def calc_dist(p1, p2):
     return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
 
@@ -15,14 +23,17 @@ def slope_and_intercept(p1, p2):
 
 def circle_to_line(c1, r1, l1, l2):
     if l1[0] == l2[0]:  # vertical line
-        dist = abs(l1[0] - c1[0])
-        return dist <= r1
+        y1, y2 = sorted([l1[1], l2[1]])  # get lower and higher y value
+        limit_y = limit(y1, y2, c1[1])
+        return calc_dist(c1, (l1[0], limit_y)) <= r1
     elif l1[1] == l2[1]:  # horizontal
-        dist = abs(l1[1] - c1[1])
-        return dist <= r1
+        x1, x2 = sorted([l1[0], l2[0]])  # get lower and higher x value
+        limit_x = limit(x1, x2, c1[0])
+        return calc_dist(c1, (limit_x, l1[1])) <= r1
     else:  # non-straight line
         slope, intercept = slope_and_intercept(l1, l2)
         int_x = (-slope * intercept + slope * c1[1] + c1[0]) / (slope ** 2 + 1)
-        int_x = max(min(int_x, l2[0]), l1[0])  # limit to l1[0] <= int_x <= l2[0], finite line
+        x1, x2 = sorted([l1[0], l2[0]])  # get lower and higher x value
+        int_x = limit(x1, x2, int_x)
         int_y = slope * int_x + intercept
         return calc_dist((int_x, int_y), c1) <= r1
--- driver_evolution/simulation.py
+++ driver_evolution/simulation.py
@@ -8,20 +8,30 @@ lifespan = 1800
 mutation_rate = 0.01
 starting_position = [125, 250]
 walls = [
+    # walls
     [[100, 200], [600, 200]],
     [[100, 300], [600, 300]],
     [[100, 200], [100, 300]],
-    [[600, 200], [600, 300]]
+    [[600, 200], [600, 300]],
+    # obstacles
+    [[300, 200], [300, 250]],
+    [[400, 300], [400, 250]]
 ]
 checkpoint = [[575, 250], 10]
 population = Population.from_random(num_drivers, starting_position, lifespan)
 amount_alive = 0
+amount_completed = 0
 frame_count = 0
 generation_num = 1
+
+pygame.init()
+pygame.font.init()
 
 width, height = 700, 500
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Driver Evolution")
+
+font = pygame.font.SysFont("Monospace", 20)
 
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
@@ -34,12 +44,20 @@ def draw_driver(driver):
     pygame.draw.line(display, BLACK, center, vector.sub(center, dir_vec), 2)
 
 
+def draw_text(text, color, position):
+    text_surface = font.render(text, True, color)
+    display.blit(text_surface, position)
+
+
 def draw():
     pygame.draw.rect(display, BLACK, [0, 0, width, height])
 
+    draw_text(f"Generation {generation_num}", WHITE, (15, 15))
+    draw_text(f"{(amount_completed / num_drivers):.2f}% completed", WHITE, (15, 50))
+
     for wall in walls:
         p1, p2 = wall
-        pygame.draw.line(display, WHITE, p1, p2, 2)
+        pygame.draw.line(display, WHITE, p1, p2, 1)
 
     pygame.draw.circle(display, WHITE, checkpoint[0], SIZE)
 
@@ -58,16 +76,20 @@ while running:
             running = False
 
     for driver in population.drivers:
-        if driver.get_status(walls, checkpoint) == ALIVE:
+        status = driver.get_status(walls, checkpoint)
+        if status == ALIVE:
             driver.apply_force(driver.dna[frame_count])
             driver.update()
             amount_alive += 1
+        elif status == COMPLETED:
+            amount_completed += 1
 
     if amount_alive != 0 and frame_count < lifespan:
         frame_count += 1
     else:
         population.breed_next_gen(starting_position, checkpoint, mutation_rate)
         generation_num += 1
+        amount_completed = 0
         frame_count = 0
 
     amount_alive = 0
